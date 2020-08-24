c84e19feca25e1e631be328a21ab5c91b65a5c7d
Seth Mariscotes <seth.mariscotes@yahoo.com>
2020-08-24 15:47:41 +0000

    Automatically backed up by Learn

--- spec/grandma_spec.rb
+++ spec/grandma_spec.rb
@@ -1,4 +1,5 @@
 describe '#speak_to_grandma' do
+  if
   it 'responds with I LOVE YOU TOO PUMPKIN! if you say I LOVE YOU GRANDMA!' do
     expect(speak_to_grandma('I LOVE YOU GRANDMA!')).to eq 'I LOVE YOU TOO PUMPKIN!'
   end
